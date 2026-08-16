6bad813374994fc068690eaa7314627a4a0afa81
interaktyvimatematika <ftomuks@gmail.com>
2026-08-16 19:21:04 +0300

    Add files via upload

--- math-semantic-input.js
+++ math-semantic-input.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = 'P1.7.9.49-P3.2.6.7';
+  const VERSION = 'P1.7.9.49-P3.2.6.7.1';
   const KNOWN_FUNCTIONS = new Set(['sqrt']);
 
   function normalizeSource(value) {
